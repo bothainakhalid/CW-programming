0af9cddce348c9b6aa1f68551c293b28a9d3e0c3
bothaina <bm2400764@tkh.edu.eg>
2025-12-13 13:56:35 +0200

    errors

--- code.py
+++ code.py
@@ -4,10 +4,10 @@ def read_picture_file(picture_file):
     except:
         return None,None,None,None, "this file does not exist"
     
-    values = []
+    info = []
 
     for line in file:
-        line = line.strip()
+        line = line.strip() 
         if line == "":
             continue
         if line.startswith("#"):
@@ -19,6 +19,44 @@ def read_picture_file(picture_file):
 
         separated = line.split()
         for s in separated:
-            values.append(s)
+            info.append(s)
+
+    file.close()
+
+    if len(info)<4:
+        return None,None,None,None, "header is incomplete"
+
+    if info[0] != "P3":
+        return "your picture should be in P3 PPM format"
+
+    try:
+        width = int(info[1])
+        height = int(info[2])
+    except:
+        return None, None, None,None, "invalid dimensions"
+
+    if width<=0 or height <=0:
+        return None,None,None,None, "invalid dimensions"
+
+    try:
+        max_colour_intensity = int(info[3])
+    except:
+        return None,None,None,None, "invalid maximum colour intensity"
+
+    pixel_values = info[4:]
+    pixels = []
+
+    for v in pixel_values:
+        if v.isdigit():
+            pixels.append(int(v))
+
+    if len(pixels) % 3 != 0:
+        return None,None,None,None, "pixel values are incomplete"
+
+
+
+
+
+    
 
 
