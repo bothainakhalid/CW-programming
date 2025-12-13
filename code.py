925f22ddd3d1f101a11c5f078e96fddcb9535779
bothaina <bm2400764@tkh.edu.eg>
2025-12-13 03:19:06 +0200

    extracting only useful data

--- code.py
+++ code.py
@@ -4,3 +4,21 @@ def read_picture_file(picture_file):
     except:
         return None,None,None,None, "this file does not exist"
     
+    values = []
+
+    for line in file:
+        line = line.strip()
+        if line == "":
+            continue
+        if line.startswith("#"):
+            continue
+        if "#" in line:
+            line = line.split("#")[0].strip()
+        if line == "":
+            continue
+
+        separated = line.split()
+        for s in separated:
+            values.append(s)
+
+
