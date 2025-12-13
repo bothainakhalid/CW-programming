93dd959463477d574d937d4c3838bbe85f824525
bothaina <bm2400764@tkh.edu.eg>
2025-12-13 20:43:19 +0200

    reads and returns picture file info

--- encoding.py
+++ encoding.py
@@ -1,3 +1,12 @@
 from functions import read_picture_file,save_pic_info, len_to_bits,byte_to_bits
 
+def encoding():
+    picture_file = input("enter the name of your picture file")
+    result = read_picture_file(picture_file)
+    pixels = result[0]
+    picture_width = result[1]
+    picture_height = result[2]
+    max_colour_intensity = result[3]
+    error = result [4]
+    
 
