8860577fb943f64e4e105e14b0a43ccc52e3fe9a
bothaina <bm2400764@tkh.edu.eg>
2025-12-14 00:49:28 +0200

    fix runtime error

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from decoding import decoding
 choice = input("if you want to ENCODE a secret message please enter '1'\n if you would like to DECODE a secret message please enter '2' " )
  
 if choice == "1":
-    encode()
+    encoding()
 elif choice == "2":
-    decode()
+    decoding()
 else:
     print("please choose 1 or 2")
 
